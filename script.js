5232cddaea2713c2c07b2d0b201212d3de8bb4ee
WebDev DM <moralesmattia9@gmail.com>
2024-10-25 16:40:14 +0200

    messaggio_console

---
+++ script.js
@@ -0,0 +1,3 @@
+// Cambia automaticamente il titolo della pagina
+document.title = 'Nuovo Titolo della Pagina';
+console.log('Ciao, questo è il mio primo messaggio dalla console in un file esterno!');
