37208fb86be9a99fee044c9114a4d0ec406e6b39
WebDev DM <moralesmattia9@gmail.com>
2024-10-25 16:09:36 +0200

    titolo-dinamico

--- script.js
+++ script.js
@@ -1,1 +1,2 @@
-console.log('Ciao, questo è il mio primo messaggio dalla console in un file esterno!');
+// Cambia automaticamente il titolo della pagina
+document.title = 'Nuovo Titolo della Pagina';
